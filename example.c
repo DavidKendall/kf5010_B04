10fde408e837b013b250c0af619245ec1fc93d52
DavidKendall <david.kendall@northumbria.ac.uk>
2016-10-10 23:02:47 +0100

    B04 2016-17

--- example.c
+++ example.c
@@ -1,93 +1,108 @@
 #include <stdbool.h>
 #include <ucos_ii.h>
-#include <osutils.h>
-#include <bsp.h>
-#include <leds.h>
+#include "gpioPin.h"
 
 /*
-********************************************************************************
-*                            APPLICATION TASK PRIORITIES
-********************************************************************************
+*********************************************************************************************************
+*                                            APPLICATION TASK PRIORITIES
+*********************************************************************************************************
 */
 
-enum {
-  APP_TASK_LINK_PRIO = 4,
-  APP_TASK_CONNECT_PRIO 
-}
+typedef enum {
+  APP_TASK_LED1_PRIO = 4,
+  APP_TASK_LED2_PRIO
+} taskPriorities_t;
 
 /*
-********************************************************************************
-*                            APPLICATION TASK STACKS
-********************************************************************************
+*********************************************************************************************************
+*                                            APPLICATION TASK STACKS
+*********************************************************************************************************
 */
 
-enum {
-  APP_TASK_LINK_STK_SIZE    = 256,
-  APP_TASK_CONNECT_STK_SIZE = 256 
-}
+#define  APP_TASK_LED1_STK_SIZE              256
+#define  APP_TASK_LED2_STK_SIZE              256
 
-static OS_STK appTaskLinkStk[APP_TASK_LINK_STK_SIZE];
-static OS_STK appTaskConnectStk[APP_TASK_CONNECT_STK_SIZE];
+static OS_STK appTaskLED1Stk[APP_TASK_LED1_STK_SIZE];
+static OS_STK appTaskLED2Stk[APP_TASK_LED2_STK_SIZE];
 
 /*
-********************************************************************************
-*                            APPLICATION FUNCTION PROTOTYPES
-********************************************************************************
+*********************************************************************************************************
+*                                            APPLICATION FUNCTION PROTOTYPES
+*********************************************************************************************************
 */
 
-static void appTaskLinkLed(void *pdata);
-static void appTaskConnectLed(void *pdata);
+static void appTaskLED1(void *pdata);
+static void appTaskLED2(void *pdata);
 
 /*
-********************************************************************************
-*                            GLOBAL FUNCTION DEFINITIONS
-********************************************************************************
+*********************************************************************************************************
+*                                            GLOBAL TYPES AND VARIABLES
+*********************************************************************************************************
 */
+
+typedef enum {
+	LED1 = 0,
+	LED2
+} deviceNames_t;
+
+gpioPin_t pin[2];
+
+/*
+*********************************************************************************************************
+*                                            GLOBAL FUNCTION DEFINITIONS
+*********************************************************************************************************
+*/
+
 int main() {
 
-  /* Initialise the board support package and the OS */
-  bspInit();
-  OSInit();                                                   
+  /* Initialise the GPIO pins */
+	gpioPinInit(&pin[LED1], 1, 18, OUTPUT_PIN);
+	gpioPinInit(&pin[LED2], 0, 13, OUTPUT_PIN);
+
+  /* Initialise the OS */
+  OSInit();                                                  
 
   /* Create the tasks */
-  OSTaskCreate(appTaskLinkLed,                               
+  OSTaskCreate(appTaskLED1,                              
                (void *)0,
-               (OS_STK *)&appTaskLinkStk[APP_TASK_LINK_STK_SIZE - 1],
-               APP_TASK_LINK_PRIO);
-  
-  OSTaskCreate(appTaskConnectLed,                               
+               (OS_STK *)&appTaskLED1Stk[APP_TASK_LED1_STK_SIZE - 1],
+               APP_TASK_LED1_PRIO);
+ 
+  OSTaskCreate(appTaskLED2,                              
                (void *)0,
-               (OS_STK *)&appTaskConnectStk[APP_TASK_CONNECT_STK_SIZE - 1],
-               APP_TASK_CONNECT_PRIO);
+               (OS_STK *)&appTaskLED2Stk[APP_TASK_LED2_STK_SIZE - 1],
+               APP_TASK_LED2_PRIO);
 
-
+ 
   /* Start the OS */
-  OSStart();                                                  
-  
-  /* Should never arrive here */ 
-  return 0;      
+  OSStart();                                                 
+ 
+  /* Should never arrive here */
+  return 0;     
 }
+
 /*
-*********************************************************************************
-*                             APPLICATION TASK DEFINITIONS
-*********************************************************************************
+*********************************************************************************************************
+*                                            APPLICATION TASK DEFINITIONS
+*********************************************************************************************************
 */
-static void appTaskLinkLed(void *pdata) {
+
+static void appTaskLED1(void *pdata) {
   /* Start the OS ticker -- must be done in the highest priority task */
-  osStartTick();
-  
+  SysTick_Config(SystemCoreClock / OS_TICKS_PER_SEC);
+ 
   /* Task main loop */
   while (true) {
-    ledToggle(USB_LINK_LED);
+    gpioPinToggle(&pin[LED1]);
     OSTimeDlyHMSM(0,0,0,500);
   }
 }
 
-static void appTaskConnectLed(void *pdata) {
+static void appTaskLED2(void *pdata) {
   while (true) {
+    gpioPinToggle(&pin[LED2]);
     OSTimeDlyHMSM(0,0,0,500);
-    ledToggle(USB_CONNECT_LED);
-  } 
+  }
 }
 
 
